c84ed74dde5f407e61450420b1a9a71a2a33b1aa
Sei Munkhbat <sei@Seis-MacBook-Pro.local>
2024-05-08 14:54:53 -0500

    Removed test title

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const dbPath = path.join(__dirname, 'db/db.json');
 
 // Middleware to parse JSON request bodies
